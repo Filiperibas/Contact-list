eca50790ffb805a378826205480de9de385324d8
Filiperibas <fellps77.plo@gmail.com>
2025-03-03 14:27:37 -0400

    Implementação do Try Except para o tratamento de erros.

--- Contact list/Contact list.py
+++ Contact list/Contact list.py
@@ -13,6 +13,8 @@ def visualizar_contato(contatos):
     return
 
 def editar_contatos(contatos, indice, propriedades):
+    propriedades = int(propriedades)
+
     if propriedades == 1:
        novo_nome_contato = input("Qual vai ser o novo nome do contato: ")
     elif propriedades == 2:
@@ -22,7 +24,7 @@ def editar_contatos(contatos, indice, propriedades):
     else:
         print("Você digitou uma opção invalida")
 
-    indice_corrigido = indice - 1
+    indice_corrigido = int(indice) - 1
     
     if propriedades == 1:
         contatos[indice_corrigido]["nome"] = novo_nome_contato
@@ -35,7 +37,9 @@ def editar_contatos(contatos, indice, propriedades):
         print(f"Email {novo_email_contato} atualizado com sucesso")
     return
 
-def favoritar_desfavoritar_contatos(contatos):
+def favoritar_desfavoritar_contatos(contatos, estado):
+    estado = int(estado)
+
     if estado == 1:
         a = "favoritar"
     elif estado == 2:
@@ -64,9 +68,8 @@ def visualizar_contato_favorito(contatos):
             print(f"{indice} {nome_contato} {favorito}")
     return
 
-def apagar_contato(contatos):
-    indice = int(input("Digite o número do contato que deseja excluir: ")) 
-    indice_corrigido = indice - 1
+def apagar_contato(contatos, indice):
+    indice_corrigido = int(indice) - 1
     contatos.pop(indice_corrigido)
     print(f"Contato {indice} removido com sucesso.")
     return 
@@ -86,38 +89,77 @@ while True:
     escolha = int(input("Digite o numero da opção desejada: "))
 
     if escolha == 1:
-        nome_contato = input("\nQue nome deseja salvar nesse contato: ")
-        telefone_contato = int(input("Insira o numero de telefone do contato: "))
-        email_contato = input("Insira o email do contato: ")
-        adicionar_contatos(contatos, nome_contato, telefone_contato, email_contato)
+        try:
+            nome_contato = input("\nQue nome deseja salvar nesse contato: ")
+            if not nome_contato.isalpha():
+                raise ValueError("O nome deve conter apenas letras!")
+
+            telefone_contato = input("Insira o numero de telefone do contato: ")
+            if not telefone_contato.isdigit():
+                raise ValueError("O telefone deve conter apenas números!")
+            
+            email_contato = input("Insira o email do contato: ")
+            if "@" not in email_contato or "." not in email_contato:
+                raise ValueError("O email inserido não é valido!")
+       
+            adicionar_contatos(contatos, nome_contato, telefone_contato, email_contato)
+        
+        except ValueError as e:
+            print(f"Houve um erro: {e}")
 
     elif escolha == 2:
         visualizar_contato(contatos)
 
     elif escolha == 3:
         visualizar_contato(contatos)
-        indice = int(input("\nQual o numero do contato que você quer editar: "))
-        print("Qual propriedade do contato você quer alterar: ")
-        print("\n1. Nome.")
-        print("2. Número.")
-        print("3. Email.")
-        propriedades = int(input())
-        editar_contatos(contatos, indice, propriedades)
+        try:
+            indice = input("\nQual o numero do contato que você quer editar: ")
+            if not indice.isdigit():
+                raise ValueError("Houve um erro, digite um número valido!")
+
+            print("Qual propriedade do contato você quer alterar: ")
+            print("\n1. Nome.")
+            print("2. Número.")
+            print("3. Email.")
+
+            propriedades = (input())
+            if not propriedades.isdigit():
+                raise ValueError("Houve um erro, digite um número valido!")
+
+            editar_contatos(contatos, indice, propriedades)
+        
+        except ValueError as e:
+            print(f"Houve um erro: {e}")
 
     elif escolha == 4:
         print("Selecione uma opção:")
         print("\n1. Favoritar contato")
         print("2. Desfavoritar contato")
-        estado = int(input())
-        visualizar_contato(contatos)
-        favoritar_desfavoritar_contatos(contatos)
+        try:
+            estado = input()
+            if not estado.isdigit():
+                raise ValueError("Houve um erro, digite apenas números!")
+               
+            visualizar_contato(contatos)
+            favoritar_desfavoritar_contatos(contatos, estado)
+        
+        except ValueError as e:
+            print(f"Houve um erro: {e}")
 
     elif escolha == 5: 
         visualizar_contato_favorito(contatos)
 
     elif escolha == 6:
         visualizar_contato(contatos)
-        apagar_contato(contatos)
+        try:
+            indice = input("Digite o número do contato que deseja excluir: ")
+            if not indice.isdigit():
+                raise ValueError("Houve um erro, digite apenas ")
+
+            apagar_contato(contatos, indice)
+
+        except ValueError as e:
+            print(f"Houve um erro: {e}")
        
     elif escolha == 7:
         break
